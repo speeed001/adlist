0240e7e67864c7739b7a89614c75915c6bf4a464
speeed01 <894846254@qq.com>
2026-06-08 05:29:14 +0800

    Update generate.ps1

--- generate.ps1
+++ generate.ps1
@@ -1,4 +1,4 @@
-# 下载 easylistchina.txt 文件（不需要代理，GitHub Actions 有公网出口）
+# 下载 easylistchina.txt 文件
 $fileUrl = "https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt"
 $outputFile = "easylistchina.txt"
 
@@ -8,30 +8,29 @@ Invoke-WebRequest -Uri $fileUrl -OutFile $outputFile
 $inputFile = "easylistchina.txt"
 $outputFilteredFile = "easylist.txt"
 
-Get-Content $inputFile | Where-Object {
-    ($_ -notmatch '(/|@@|#|!|_|\$|=|\[|\^\*|\*\.js|\*\.gif|\*\.htm|\*\.html|:|\|\|sax\*\.sina\.)') -and ($_ -match '^\|\|')
-} | Set-Content $outputFilteredFile
+$rules = Get-Content $inputFile | Where-Object {
+    ($_ -notmatch '(/|@@|#|!|_|\$|=|\[|\^\*|\*\.js|\*\.gif|\*\.htm|\*\.html|:|\|\|sax\*\.sina\.)') -and
+    ($_ -match '^\|\|')
+} | Sort-Object -Unique
+
+# 规则数量
+$ruleCount = $rules.Count
 
 # 获取当前日期
 $currentDate = Get-Date -Format "yyyy-MM-dd"
 
-# 定义文件头内容
+# 文件头
 $headerContent = @"
 ! Title: easylistchinalite-dns
 ! TimeUpdated: $currentDate
-! Description:  easylistchina精简
+! 规则数量: $ruleCount
+! Description: easylistchina精简
 
 "@
 
-# 合并头部和内容
-$fileContent = Get-Content -Path $outputFilteredFile
-$newContent = $headerContent + "`n" + ($fileContent -join "`n")
-
 # 写入最终文件
-#$newContent | Set-Content -Path $outputFilteredFile
-$newContent | Set-Content -Path $outputFilteredFile -Encoding UTF8
+$headerContent | Set-Content -Path $outputFilteredFile -Encoding UTF8
+$rules | Add-Content -Path $outputFilteredFile -Encoding UTF8
 
-
-
-
-
+Write-Host "生成完成: $outputFilteredFile"
+Write-Host "规则数量: $ruleCount"
